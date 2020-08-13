73a5abdc78efbce1103168a4e93ca0c284ca714c
Andrew Norman <anorman728@gmail.com>
2020-08-12 20:24:51 -0500

    Moved testing to subfolder.

--- testing/test-setup.php
+++ testing/test-setup.php
@@ -6,7 +6,7 @@ if (!isset($outputfile)) {
 
 $basedir = __DIR__ . '/';
 
-include_once($basedir . 'phlute.php');
+include_once($basedir . '../phlute.php');
 print_r("Ignore preceeding warning.  Proceeding with test.\n");
 
 $outputfileFull = $basedir . 'test-output/' . $outputfile;
